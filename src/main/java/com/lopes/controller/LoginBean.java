c96ffb6f85fdf85056147eba22c800c16369726d
MairaCelestino <mairacelestino@hotmail.com>
2020-01-30 14:49:49 +0000

    The LoginBean class has been modified: "autenticar ()" method: First validates user if the result is true, validates the password.

--- src/main/java/com/lopes/controller/LoginBean.java
+++ src/main/java/com/lopes/controller/LoginBean.java
@@ -11,50 +11,61 @@ import javax.inject.Named;
 @SessionScoped
 public class LoginBean implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private String password;
-    private String user;
+	private String password;
+	private String user;
 
-    public String autenticar()
-    {
-        if (this.getUser().trim().equalsIgnoreCase("ADMIN") && this.getPassword().equalsIgnoreCase("ADMIN"))
-            return "home?faces-redirect=true";
-        else
-        {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN,
-                    "Credenciais inválidas", "Please enter correct username and Password"));
-            return "";
-        }
-    }
+//    public String autenticar()
+//    {
+//        if (this.getUser().trim().equalsIgnoreCase("ADMIN") && this.getPassword().equalsIgnoreCase("ADMIN"))
+//            return "home?faces-redirect=true";
+//        else
+//        {
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN,
+//                    "Credenciais inválidas", "Please enter correct username and Password"));
+//            return "";
+//        }
+//    }
 
-    public String getPassword()
-    {
-        return password;
-    }
+	public String autenticar() {
+		if (this.getUser().trim().equalsIgnoreCase("ADMIN")) {
+			if (this.getPassword().equalsIgnoreCase("ADMIN")) {
+				return "home?faces-redirect=true";
+			} else {
+				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN,
+						"Invalid pasword!", "Please enter correct Password"));
+				return "";
+			}
+		} else {
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_WARN, "Invalid Username!", "Please enter correct Username"));
+			return "";
+		}
+	}
 
-    public String getUser()
-    {
-        return user;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public String logout()
-    {
-        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
-        FacesContext.getCurrentInstance().addMessage(null,
-                new FacesMessage(FacesMessage.SEVERITY_INFO, "Logout com sucesso!", ""));
-        FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
-        return "/login?faces-redirect=true";
-    }
+	public String getUser() {
+		return user;
+	}
 
-    public void setPassword(String password)
-    {
-        this.password = password;
-    }
+	public String logout() {
+		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, "Logout com sucesso!", ""));
+		FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
+		return "/login?faces-redirect=true";
+	}
 
-    public void setUser(String user)
-    {
-        this.user = user;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
 
 }
