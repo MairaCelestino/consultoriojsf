ef71f7f34c1bf5b1265b5759325c4e54abf02e89
MairaCelestino <mairacelestino@hotmail.com>
2020-03-03 00:14:26 +0000

    Finished DoctorBean class.

--- src/main/java/com/celestino/model/util/ClinicException.java
+++ src/main/java/com/celestino/model/util/ClinicException.java
@@ -2,21 +2,23 @@ package com.celestino.model.util;
 
 public class ClinicException extends Exception {
 
-    public ClinicException() {
+	private static final long serialVersionUID = 1L;
 
-    }
+	public ClinicException() {
 
-    public ClinicException(String message) {
-        super(message);
-    }
+	}
 
-    public ClinicException(Throwable message) {
-        super(message);
+	public ClinicException(String message) {
+		super(message);
+	}
 
-    }
+	public ClinicException(Throwable message) {
+		super(message);
 
-    public ClinicException(String message, Throwable message1){
-        super(message, message1);
-    }
+	}
+
+	public ClinicException(String message, Throwable message1) {
+		super(message, message1);
+	}
 
 }
